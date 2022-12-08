741d689137d23c01a7034cf5c14ad112cdafda1f
SetManAta <karabukaev.e@gmail.com>
2022-12-08 21:25:59 +0600

    Как же вы бесите со свим Shell

--- filial1/models.py
+++ filial1/models.py
@@ -34,6 +34,7 @@ class Worker(User):
 class Ingridient(models.Model):
     name = models.CharField(max_length=20)
     extra_price = models.IntegerField()
+    calories = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -43,20 +44,54 @@ class Food(models.Model):
     name = models.CharField(max_length=20)
     start_price = models.IntegerField()
     orders = models.ManyToManyField(Ingridient, related_name='food', through='Order')
+    type_of_cuisine = models.CharField(max_length=20)
+    calories = models.IntegerField()
 
     def __str__(self):
         return self.name
     
 
-    
+
+meat = {'chicken', 'beef', 'fish' }    
 
 class Order(models.Model):
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
     ingridient = models.ForeignKey(Ingridient,on_delete=models.CASCADE)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     worker = models.ForeignKey(Worker,on_delete=models.CASCADE)
+    vegetarian = models.BooleanField(default=True,null=True)
+    food_status = models.CharField(max_length=20,null=True)
+    final_price = models.IntegerField(null=True)
     order_date_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.food.name} - {self.ingridient.name} - {self.client.name} - {self.worker.name}'
 
+
+
+class Resultat(Order):
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if self.ingridient.name in ['chicken', 'beef', 'fish']:
+            self.vegetarian = False
+        else:
+            self.vegetarian = True
+        # if any([x in self.ingridient for x in meat]):
+        #     self.vegetarian = False
+
+        resultat_cal = self.food.calories + self.ingridient.calories
+
+        if resultat_cal <= 700:
+            self.food_status = 'Перекус'
+        elif resultat_cal <= 1200:
+            self.food_status = 'Обед'
+        elif resultat_cal > 1200:
+            self.food_status = 'обжиралова'
+
+        self.final_price = self.food.start_price + self.ingridient.extra_price
+        super().save(*args, **kwargs)  
+
+
+    
--- shell.py
+++ shell.py
@@ -1,42 +1,60 @@
 from filial1 .models import *
 
-# shaurma = 50
-# gamburger = 25
-# chees = 10
-# chicken = 70
-# beef = 80
-# salad = 15
-# free = 15
 
-client1 = Client(email= 'nikname21@gmail.com', password ='defender42', name = 'Азат Соколов', card_number = '4147565798789009')
+# client1 = Client(email= 'nikname21@gmail.com', password ='defender42', name = 'Азат Соколов', card_number = '4147565798789009')
 
-worker1 = Worker(email = 'altywa1998@gmail.com', password= 'nono34',name = 'Алтынай Алиева', position = 'Оператор кассы')
-client1.save()
-worker1.save()
+# worker1 = Worker(email = 'altywa1998@gmail.com', password= 'nono34',name = 'Алтынай Алиева', position = 'Оператор кассы')
+# client1.save()
+# worker1.save()
 
-ingridients_chees = Ingridient.objects.create(name='chees',extra_price=10)
-ingridients_chicken = Ingridient.objects.create(name='chicken',extra_price=70)
-ingridients_beef = Ingridient.objects.create(name='beef',extra_price=80)
-ingridients_salad = Ingridient.objects.create(name='salad',extra_price=15)
-ingridients_free = Ingridient.objects.create(name='free',extra_price=15)
+# ingridients_chees = Ingridient.objects.create(name='chees',extra_price=10)
+# ingridients_chicken = Ingridient.objects.create(name='chicken',extra_price=70)
+# ingridients_beef = Ingridient.objects.create(name='beef',extra_price=80)
+# ingridients_salad = Ingridient.objects.create(name='salad',extra_price=15)
+# ingridients_free = Ingridient.objects.create(name='free',extra_price=15)
 
-food_shaurma = Food.objects.create(name='shaurma',start_price=50)
-food_gamburger = Food.objects.create(name='gamburger',start_price=25)
+# food_shaurma = Food.objects.create(name='shaurma',start_price=50)
+# food_gamburger = Food.objects.create(name='gamburger',start_price=25)
 
 
-food_shaurma.orders.set([ingridients_beef, ingridients_chees, ingridients_salad,ingridients_free], through_defaults={'client':client1, 'worker':worker1})
-shaurma_price = food_shaurma.start_price + ingridients_beef.extra_price + ingridients_chees.extra_price + ingridients_salad.extra_price + ingridients_free.extra_price
-print(shaurma_price)
+# food_shaurma.orders.set([ingridients_beef, ingridients_chees, ingridients_salad,ingridients_free], through_defaults={'client':client1, 'worker':worker1})
+# shaurma_price = food_shaurma.start_price + ingridients_beef.extra_price + ingridients_chees.extra_price + ingridients_salad.extra_price + ingridients_free.extra_price
+# print(shaurma_price)
 
-food_gamburger.orders.set([ingridients_chicken, ingridients_salad], through_defaults={'client':client1, 'worker':worker1})
-gamburger_price = food_gamburger.start_price + ingridients_chicken.extra_price + ingridients_salad.extra_price
-print(gamburger_price)
+# food_gamburger.orders.set([ingridients_chicken, ingridients_salad], through_defaults={'client':client1, 'worker':worker1})
+# gamburger_price = food_gamburger.start_price + ingridients_chicken.extra_price + ingridients_salad.extra_price
+# print(gamburger_price)
 
-# client = Азат у Алтынай: shaurma + beef + chees + salad + free
-#                         gamburger + chicken + salad
 
-#     Выведите по отдельности стоимость:
-#     Шаурмы которую заказал Азат
 
-#     Гамбургера которую заказал Азат
-#     Общую стоимость заказа
+client1 = Client.objects.create(name= 'Нурсултан Бердиев', card_number= '4147565798789009',email = 'nikname21@gmail.com', password='defender42')
+
+worker1 = Worker.objects.create(email = 'altywa1998@gmail.com', password= 'nono34',name = 'Алтынай Алиева', position = 'Оператор кассы')
+
+
+food_shaurma = Food.objects.create(name='shaurma',start_price=200,type_of_cuisine='фастфуд',calories=500)
+food_gamburger = Food.objects.create(name='gamburger',start_price=180,type_of_cuisine='фастфуд',calories=350)
+food_pasta = Food.objects.create(name='pasta',start_price=450,type_of_cuisine='Итальянская',calories=400)
+food_boul = Food.objects.create(name='boul',start_price=600,type_of_cuisine='европейское',calories=500)
+food_sushi = Food.objects.create(name='sushi',start_price=400,type_of_cuisine='японская',calories=450)
+
+ingridients_chees = Ingridient.objects.create(name='chees',extra_price=80,calories=150)
+ingridients_chicken = Ingridient.objects.create(name='chicken',extra_price=100, calories=250)
+ingridients_beef = Ingridient.objects.create(name='beef',extra_price=120,calories=300)
+ingridients_salad = Ingridient.objects.create(name='salad',extra_price=50,calories=50)
+ingridients_free = Ingridient.objects.create(name='free',extra_price=50,calories=70)
+ingridients_fish = Ingridient.objects.create(name='fish',extra_price=120,calories=270)
+ingridients_rice = Ingridient.objects.create(name='rice',extra_price=70,calories=100)
+ingridients_tvorog = Ingridient.objects.create(name='tvorog',extra_price=100,calories=170)
+
+result_shaurma = food_shaurma.orders.set([ingridients_beef, ingridients_chees, ingridients_salad,ingridients_free], through_defaults={'client':client1, 'worker':worker1})
+for fp in Resultat.objects.all():
+    fp.save()
+
+
+
+
+
+
+
+
